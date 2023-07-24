3c17658afea7310744fbbfd5d32754846192355c
FameZeeshan <zeeshan6143@gmail.com>
2023-07-24 20:44:15 +0530

    2 4543 backend connected

--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 // this file will help to handle API from frontend to backend
 import axios from "axios";
-const baseUrl = "https://localhost:5000";
+const baseUrl = "http://localhost:5000";
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
     console.log("data------->", data);
